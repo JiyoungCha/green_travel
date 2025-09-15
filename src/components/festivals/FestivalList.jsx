c13f0a5d0d90c0d769e57f7fabf6ed50f56056aa
JiyoungCha <isasegun@gmail.com>
2025-09-15 17:56:14 +0900

    250915 FSTIVAL_LIST01 리트스 렌더링 처리 및 페이지네이션 구현

--- src/components/festivals/FestivalList.jsx
+++ src/components/festivals/FestivalList.jsx
@@ -1,38 +1,65 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './FestivalList.css';
 import { useEffect } from 'react';
 import { festivalIndex } from '../../store/thunks/festivalThunk.js';
+import { dateFormatter } from '../../utils/dateFormatter.js';
+import { setScrollEventFlg } from '../../store/slices/festivalSlice.js';
 
 function FestivalList() {
   const dispatch = useDispatch();
+  
+  const festivalList = useSelector(state => state.festival.list);
+  const page = useSelector(state => state.festival.page);
+  const scrollEventFlg = useSelector(state => state.festival.scrollEventFlg);
 
   useEffect(() => {
-    dispatch(festivalIndex());
-  }, []);
+    dispatch(festivalIndex(1))
+  }, [])
+
+  // 스크롤 이벤트
+  useEffect(() => {
+    window.addEventListener('scroll', addNextPage); 
+
+    return () => {
+      window.removeEventListener('scroll', addNextPage);
+    }
+  }, [page, scrollEventFlg]);
+
+  // 다음 페이지 가져오기
+  function addNextPage() {
+    // 스크롤 이벤트 시 주의사항
+    // 1. 굉장히 잦은 빈도로 실행됨 > 디바운싱 & 쓰로틀링 & IntersectionObserver 이용 실행 빈도 조절 필수
+    // 2. 같은 요청을 반복적으로 할 가능성이 높아, 반복적으로 실행이 안되도록 제어하는 것이 필수
+    // 스크롤 관리 처리
+    const docHeight = document.documentElement.scrollHeight; // 문서의 Y축 총 길이
+    const winHeight = window.innerHeight; // 윈도우의 Y측 총 길
+    const nowHeight = window.scrollY; // 현재 스크롤의 Y축 위치
+    const viewHeight = docHeight - winHeight; // 스크롤을 끝까지 내렸을 때의 Y축 위치
+    
+    if(viewHeight === nowHeight && scrollEventFlg) {
+      dispatch(setScrollEventFlg(false));
+      dispatch(festivalIndex(page + 1));
+    }
+  }
+    
 
   return (
     <>
     <div className="container">
-      <div className="card">
-        <div className="card-img" style={{backgroundImage: `url('http://tong.visitkorea.or.kr/cms/resource/91/3484791_image2_1.jpg')`}}></div>
-        <p className="card-title">가락몰 3층 하늘공원</p>
-        <p className="card-period">2025.09.01 - 2025.09.25</p>
-      </div>
-      <div className="card">
-        <div className="card-img" style={{backgroundImage: `url('http://tong.visitkorea.or.kr/cms/resource/91/3484791_image2_1.jpg')`}}></div>
-        <p className="card-title">가락몰 3층 하늘공원</p>
-        <p className="card-period">2025.09.01 - 2025.09.25</p>
-      </div>
-      <div className="card">
-        <div className="card-img" style={{backgroundImage: `url('http://tong.visitkorea.or.kr/cms/resource/91/3484791_image2_1.jpg')`}}></div>
-        <p className="card-title">가락몰 3층 하늘공원</p>
-        <p className="card-period">2025.09.01 - 2025.09.25</p>
-      </div>
-      <div className="card">
-        <div className="card-img" style={{backgroundImage: `url('http://tong.visitkorea.or.kr/cms/resource/91/3484791_image2_1.jpg')`}}></div>
-        <p className="card-title">가락몰 3층 하늘공원</p>
-        <p className="card-period">2025.09.01 - 2025.09.25</p>
-      </div>
+      {
+        festivalList.length > 0 && festivalList.map(item => {
+          return (
+            <div className="card" key={item.contentid + item.createdtime}>
+              <div className="card-img" style={{backgroundImage: `url('${item.firstimage}')`}}></div>
+              <p className="card-title">{item.title}</p>
+              <p className="card-period">{dateFormatter.WithHyphenYMD(item.eventstartdate)} - {dateFormatter.WithHyphenYMD(item.eventenddate)}</p>
+            </div>
+          )
+        })
+      } 
+    </div>
+    <div className=''>
+      <button className='morebutton' type='button' onClick={addNextPage}>더보기</button>
     </div>
     </>
   )  
--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,10 +1,11 @@
 const axiosConfig = {
-  serviceKey: '77a64d7f0e247e48590ab9b870408ea322854340835c427e1ab849ed6fbec50c',
-  MobileOS: 'WEB',
-  MobileApp: 'Green Tavel',
-  type: 'json',
-  arrange: 'O',
-  baseUrl: 'https://apis.data.go.kr/B551011/KorService2',
+  SERVICE_KEY: '77a64d7f0e247e48590ab9b870408ea322854340835c427e1ab849ed6fbec50c',
+  MOBILE_OS: 'WEB',
+  MOBILE_APP: 'Green Tavel',
+  TYPE: 'json',
+  ARRANGE: 'O',
+  BASE_URL: 'https://apis.data.go.kr/B551011/KorService2',
+  NUMER_OF_ROWS: 9,
 }
 
 export default axiosConfig;
